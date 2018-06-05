6adfbc41226694185c8fb0c4df6bdfc6c3b3efbc
linda@reachmaine.com <linda@reachmaine.com>
2018-06-05 15:50:33 -0400

    initial load

---
+++ barharborglamp/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+//add_action( 'after_setup_theme', 'reach_child_theme_setup' );
+
+
+// function reach_themes_child_theme_setup() {
+
+// }
+// add_action( 'init', 'be_restore_default_gallery');
+
+  require_once(get_stylesheet_directory().'/custom/branding.php');
+  require_once(get_stylesheet_directory().'/custom/language.php');
+  require_once(get_stylesheet_directory().'/custom/reach_CTAs.php');
+  require_once(get_stylesheet_directory().'/custom/oshine.php');
+
+?>
